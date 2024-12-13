a939b9987db1b99bb989af1e825978892ece318a
assemsaduakas <sukhambekovaa@gmail.com>
2024-12-13 21:24:03 +0100

    initial commit

---
+++ src/main/java/com/example/mvcwebflux/MvcWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvcwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcWebfluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mvcwebflux/MvcWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvcwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
